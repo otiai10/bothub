56acdc4a29c1c1378c4805c5ecca365afe3bb64b
otiai10 <otiai10@gmail.com>
2014-06-10 18:10:14 +0900

    Add minimum validation

--- app/controllers/api.go
+++ app/controllers/api.go
@@ -2,6 +2,7 @@ package controllers
 
 import "github.com/revel/revel"
 import "encoding/json"
+import "errors"
 
 type Api struct {
 	*revel.Controller
@@ -9,17 +10,40 @@ type Api struct {
 
 func (c Api) Index() revel.Result {
 	return c.RenderJson(map[string]string{
-        "hoge": "fuga",
-        "foo" : "bar",
-    })
+		"hoge": "fuga",
+		"foo":  "bar",
+	})
 }
 
 // curl http://localhost:9000/api/queue/add -X POST -d 'sample={"hoge":"fuga"}'
 type Sample struct {
-    Hoge string `json:"hoge"`
+	Hoge string `json:"hoge"`
 }
+type Response struct {
+	OK       bool   `json:"ok"`
+	Message  string `json:"message"`
+	Accepted Sample `json:"accepted"`
+}
+
 func (c Api) QueueAdd(sample string) revel.Result {
-    var s Sample
-    json.Unmarshal([]byte(sample), &s)
-	return c.RenderJson(s)
+	var s Sample
+	if e := json.Unmarshal([]byte(sample), &s); e != nil {
+		return c.RenderJson(Response{
+			false, e.Error(), s,
+		})
+	}
+	if e := validate(s); e != nil {
+		return c.RenderJson(Response{
+			false, e.Error(), s,
+		})
+	}
+	return c.RenderJson(Response{
+		true, "OK", s,
+	})
+}
+func validate(s Sample) (e error) {
+	if s.Hoge == "" {
+		return errors.New("Missing parameter `hoge`")
+	}
+	return
 }

38ea175a648ddcd12d09929bc6699851c9e73df6
otiai10 <otiai10@gmail.com>
2014-06-12 22:06:01 +0900

    Refactor consumer singleton

---
+++ model/consumer.go
@@ -0,0 +1,24 @@
+package model
+
+import "github.com/mrjones/oauth"
+import "github.com/revel/revel"
+
+var consumer oauth.Consumer
+var consumer_initialized = false
+
+func GetConsumer() *oauth.Consumer {
+    if ! consumer_initialized {
+        key    := revel.Config.StringDefault("twitter.consumer_key", "")
+        secret := revel.Config.StringDefault("twitter.consumer_secret", "")
+        consumer = *oauth.NewConsumer(
+            key,
+            secret,
+            oauth.ServiceProvider{
+                AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
+                RequestTokenUrl:   "https://api.twitter.com/oauth/request_token",
+                AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
+            },
+        )
+    }
+	return &consumer
+}
